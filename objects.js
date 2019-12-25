9f353947c5433d39b5ae72135322d0f1c6c030b8
Michael Nnaji <mnnaji3@uwo.ca>
2019-12-25 12:46:18 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {"burna-boy":"Ye"}
 function updatePlaylist(playlist,artistName,songTitle){
-  playlist.artistName = songTitle
+  playlist[artistName] = songTitle
   return(playlist)
 }
